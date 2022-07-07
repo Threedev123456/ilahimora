3a6dc3771593aa3b1442e158ebf6a6aa999ca7f4
developp <devdevelopp@gmail.com>
2022-07-07 08:33:00 +0200

    blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\BlogRepository;
 use App\Repository\CategoryRepository;
 use App\Repository\ImageTopBlogRepository;
+use App\Repository\ProduitsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +13,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'app_blog')]
-    public function index(BlogRepository $blogRepository,ImageTopBlogRepository $imageTopBlogRepository, CategoryRepository $categoryRepository): Response
+    public function index(BlogRepository $blogRepository,ImageTopBlogRepository $imageTopBlogRepository, CategoryRepository $categoryRepository, ProduitsRepository $produitsRepository): Response
     {
+        $produit = $produitsRepository->findAll();
         $category = $categoryRepository->findAll();
         $imageTopBlogs =$imageTopBlogRepository->findAll();
         $blogs = $blogRepository->findAll();
@@ -21,6 +23,7 @@ class BlogController extends AbstractController
             'blogs' => $blogs,
             'imageTopBlogs' => $imageTopBlogs,
             'category'=>$category,
+            'produit'=> $produit,
         ]);
     }
 }
